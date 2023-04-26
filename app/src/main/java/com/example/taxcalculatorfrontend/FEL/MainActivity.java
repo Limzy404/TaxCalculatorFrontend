57979f20e7e10082baced6e53992f9e69c287cd5
Limzy404 <lisimard75@gmail.com>
2023-04-26 19:52:18 -0400

    hint

--- app/src/main/java/com/example/taxcalculatorfrontend/FEL/MainActivity.java
+++ app/src/main/java/com/example/taxcalculatorfrontend/FEL/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.taxcalculatorfrontend.FEL;
 
+import android.app.AlertDialog;
 import android.content.Intent;
+import android.media.Image;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
@@ -10,6 +12,7 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
@@ -27,6 +30,7 @@ public class MainActivity extends AppCompatActivity {
     private EditText mainInputGross;
     private EditText mainInputIP;
     private Button mainBtnCalculate;
+    private ImageView mainImgInfo;
     String apiString;
     Double amount;
     Handler handler = new Handler(Looper.getMainLooper(), new Handler.Callback() {
@@ -57,12 +61,20 @@ public class MainActivity extends AppCompatActivity {
                 startAPI();
             }
         });
+
+        mainImgInfo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                infoAlert();
+            }
+        });
     }
 
     private void initView() {
         mainInputGross = (EditText) findViewById(R.id.main_input_gross);
         mainBtnCalculate = (Button) findViewById(R.id.main_btn_calculate);
         mainInputIP = (EditText) findViewById(R.id.main_input_ip);
+        mainImgInfo = (ImageView)  findViewById(R.id.main_img_info);
     }
     public void startAPI(){
         String str = mainInputGross.getText().toString();
@@ -78,6 +90,17 @@ public class MainActivity extends AppCompatActivity {
             Toast.makeText(getApplicationContext(), "Enter gross amount...", Toast.LENGTH_SHORT).show();
             Log.e("ERROR","Cannot parse string as double");
         }
+    }
+
+    public void infoAlert(){
+        new AlertDialog.Builder(this)
+                .setTitle("IP Configuration")
+                .setMessage("Use this to configure the server IP.\n\n" +
+                        "Default 10.0.2.2 works with an Android Virtual Machine in Android Studio.\n\n" +
+                        "Hint: Get your server's ipv4 to run remotely from a server on the local network.")
+                .setNegativeButton("Done",null)
+                .setIcon(android.R.drawable.ic_dialog_info)
+                .show();
     }
 }
 
